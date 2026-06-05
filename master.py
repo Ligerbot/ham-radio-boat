77d1a1012a57f3c34324b1118c4d5d72e1219c1d
Ligerbot <ilan@snackmail.cc>
2026-06-04 22:01:43 -0700

    added more to master.py

--- master.py
+++ master.py
@@ -1,1 +1,4 @@
 #this script will be like the "main loop" of the boat. It will manage the programs in all the subfolders.
+#the main script in each subfolder must expose a function and be importable like this:
+#import analog.sstv as sstv ... or:
+#import digital.fsk as fsk
