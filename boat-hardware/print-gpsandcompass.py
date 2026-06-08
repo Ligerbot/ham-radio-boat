401facf3d9a999606d63bf37ba97385038feae32
KO6MUM <nathansacerdote@gmail.com>
2026-06-08 12:18:57 -0700

    Create print-gpsandcompass.py
    Uses python print() function to output the data from the Arduino's serial.print() function into the rPi's terminal. This can be updated in the future to be used to transfer any data from the Arduino to the rPi.

---
+++ boat-hardware/print-gpsandcompass.py
@@ -0,0 +1,10 @@
+# When compassandgps.ino is running on the arduino, and the arduno is connected to the rPi via USB cable, this program has the rPi print what the arduino outputs through serial.print
+
+import serial, time
+
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=5)
+
+start = time.time()
+while time.time() - start < 60:
+    print(ser.readline().decode(errors='ignore').strip())
+ser.close()
