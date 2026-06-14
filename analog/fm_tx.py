7a28be6cbb16c2aa8a42b93ae4e0746c0a814ca9
Ligerbot <ilan@snackmail.cc>
2026-06-13 17:47:52 -0700

    Progress on transmitting FM from the GPIO pin of a raspberry pi

--- analog/fm_tx.py
+++ analog/fm_tx.py
@@ -2,14 +2,21 @@ import subprocess
 from mutagen.wave import WAVE
 import time
 
-audio = WAVE("input.wav")
-broadcast = subprocess.Popen(["sudo", "./testnfm.sh", "434", "input.wav"],
-	stdin=subprocess.PIPE,
-	stderr=subprocess.DEVNULL
-)
-timenow = audio.info.length
-time.sleep(timenow)
-broadcast.terminate()
+def tx(wavfile):
+	audio = WAVE(str(wavfile))
+	#TODO: make sure this program doesn't get stuck with the transmitter on
+	#also TODO: bandpass filter so rpitx doesn't kill people with pacemakers
+	#another TODO: make it so it doesn't ask the sudo password eveyr time or any time at all
 
-#runs sudo ./testnfm.sh 434 src/resources/SAMPLE_MONO_AUDIO.wav
+	broadcast = subprocess.Popen(["sudo", "./testnfm.sh", "434", str(wavfile)],
+		stdin=subprocess.PIPE,
+		stderr=subprocess.DEVNULL
+	)
+	timenow = audio.info.length #get the length of the file to play
+	time.sleep(timenow) #block
+	broadcast.terminate() #terminate once file is done
+
+
+#this library is basically a wrapper for the following command:
+#sudo ./testnfm.sh 434 src/resources/SAMPLE_MONO_AUDIO.wav
 
